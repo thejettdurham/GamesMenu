60e59642a7b9573c4f2f08277f63e8b75e4fc5c9
Jett Durham <jett@jettdurham.com>
2017-07-11 22:33:26 -0500

    Fully implemented reducers

--- app/lib/appState.js
+++ app/lib/appState.js
@@ -38,6 +38,7 @@ export default class AppState {
     };
 
     ItemContainer: {
+        LastScreenId?: number,
         ActiveScreenId?: number
     };
 
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,17 +1,32 @@
-import { Selection } from '../lib/appState';
+import { Selection, } from '../lib/appState';
 import { Alert } from 'react-native';
 import * as types from '../actions/types';
 
 export default appStateReducer = (state, action) => {
     switch (action.type) {
         case types.CONFIRM_SELECTION: {
-            // Append Active Selection to Confirmed Selections
-            // Remove Active Selection
-            // Remove SelectedGroupId
-            // Remove ActiveScreenID
-            // Disable nav buttons
-            return state;
+            return Object.assign({}, state, {
+                SelectionContainer: {
+                    ConfirmedSelections: [
+                        ...state.SelectionContainer.ConfirmedSelections,
+                        state.SelectionContainer.ActiveSelection,
+                    ],
+                },
+                GroupContainer: {
+                    
+                },
+                ItemContainer: {
+
+                },
+                NavContainer: {
+                    ButtonIsEnabled: {
+                        Undo: false,
+                        Accept: false,
+                    }
+                }
+            });
         }
+
         case types.DELETE_SELECTION: {
             let filteredSelections = state.SelectionContainer.ConfirmedSelections.filter((_, i) => i !== action.index);
 
@@ -22,6 +37,7 @@ export default appStateReducer = (state, action) => {
                 }
             });
         }
+
         case types.SET_SELECTED_GROUP: {
             // View logic ensures that old groupId !== action.groupId
             if (state.GroupContainer.SelectedGroupId === action.groupId) return state;
@@ -58,25 +74,62 @@ export default appStateReducer = (state, action) => {
             });
         }
         case types.SET_ITEM: {
-            // Add chosen ItemId to ActiveSelection
-            // Set LastScreenId = ActiveScreenId
-            // If item has children, set ActiveScreen to ItemId
-            return state;
+            let modifiedSelection = Object.assign({}, state.SelectionContainer.ActiveSelection, {
+               ItemId: action.itemId,
+            });
+
+            return Object.assign({}, state, {
+                SelectionContainer: {
+                    ConfirmedSelections: state.SelectionContainer.ConfirmedSelections,
+                    ActiveSelection: modifiedSelection,
+                },
+                ItemContainer: {
+                    // Items with no modifiers are handled in the view layer
+                    ActiveScreenId: action.itemId,
+                },
+                NavContainer: {
+                    ButtonIsEnabled: {
+                        Undo: true,
+                        Accept: true,
+                    }
+                }
+            });
         }
+
         case types.SET_MODIFIER_GROUP: {
-            // If item has children, set ActiveScreen to ModGroupId
-            return state;
+            return Object.assign({}, state, {
+                ItemContainer: {
+                    LastScreenId: state.ItemContainer.ActiveScreenId,
+                    ActiveScreenId: action.modGroupId,
+                }
+            });
         }
+
         case types.SET_MODIFIER: {
+            let modifiedSelection = Object.assign({}, state.SelectionContainer.ActiveSelection, {
+                Modifiers: [
+                    ...state.SelectionContainer.ActiveSelection.Modifiers,
+                    action.modId
+                ],
+            });
+
+            return Object.assign({}, state, {
+                SelectionContainer: {
+                    ConfirmedSelections: state.SelectionContainer.ConfirmedSelections,
+                    ActiveSelection: modifiedSelection,
+                },
+                ItemContainer: {
+                    LastScreenId: state.ItemContainer.ActiveScreenId,
+                    // Items with no modifiers are handled in the view layer
+                    ActiveScreenId: action.modId,
+                }
+            });
+
             // Append modifierId to modifiers array of active selection
             // Set ActiveScreenId = LastScreenId
             // Set LastScreenId = ActiveScreenId
-            return state;
         }
-        case types.UNDO_LAST_MOD: {
-            // TODO need to use redux-undo's undo action creator here
-            return state.past;
-        }
+
         default: return state;
     }
 };
